81625973d988ce954c9181b5715322ad3be60bd5
Shubham <sb25.m3@gmail.com>
2020-04-21 18:29:07 +0530

    set data

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -16,47 +16,98 @@ export class ProductService {
       name: 'Milkfood Rich Desi Daneder Ghee',
       mrp: '233',
       rs: '32',
-      off: '20'
+      off: '20',
+      img:'https://n3.sdlcdn.com/imgs/e/l/1/MSG-Cow-Desi-Ghee-1-SDL595875615-1-fddf8.png'
     },
     {
       _id: 2,
-      brand: 'Milkfood',
-      name: 'Milkfood Rich Desi Daneder Ghee',
-      mrp: '233',
-      rs: '32',
-      off: '20'
+      brand: 'Fortune',
+      name: 'Fortune Sunlite Refined Sunflower Oil Pouch',
+      mrp: '120',
+      rs: '98',
+      off: '18',
+      img:'https://rukminim1.flixcart.com/image/832/832/k7usyvk0/edible-oil/e/t/c/1-sunlite-refined-pouch-sunflower-oil-fortune-original-imafpzwhyyhhvzbk.jpeg?q=70'
     },
     {
       _id: 3,
-      brand: 'Milkfood',
-      name: 'Milkfood Rich Desi Daneder Ghee',
-      mrp: '233',
-      rs: '32',
-      off: '20'
+      brand: 'Hambre',
+      name: 'Hambre Raisin_Golden_250gm Raisins  (250 g)',
+      mrp: '430',
+      rs: '245',
+      off: '43',
+      img: 'https://rukminim1.flixcart.com/image/832/832/jp02t8w0/nut-dry-fruit/2/c/3/250-raisin-golden-250gm-pouch-hambre-original-imafb9yprzn7jrg8.jpeg?q=70'
     },
     {
       _id: 4,
-      brand: 'Milkfood',
-      name: 'Milkfood Rich Desi Daneder Ghee',
-      mrp: '233',
-      rs: '32',
-      off: '20'
+      brand: 'TATA',
+      name: 'Tata Iodized Salt  (1 kg)',
+      mrp: '20',
+      rs: '18',
+      off: '10',
+      img: 'https://rukminim1.flixcart.com/image/832/832/jq4353k0/salt/q/w/m/1-na-iodized-salt-tata-original-imafc7agxkbypvgr.jpeg?q=70'
     },
     {
       _id: 5,
-      brand: 'Milkfood',
-      name: 'Milkfood Rich Desi Daneder Ghee',
-      mrp: '233',
-      rs: '32',
-      off: '20'
+      brand: 'Cadbury',
+      name: 'Cadbury Bournvita Health Nutrition Drink  (500 g, Chocolate Flavored)',
+      mrp: '217',
+      rs: '210',
+      off: '3',
+      img: 'https://rukminim1.flixcart.com/image/832/832/jr0y9ow0/energy-sport-drink-mix/m/r/w/500-pro-health-chocolate-cadbury-bournvita-original-imafcx6zg3sazzzk.jpeg?q=70'
     },
     {
       _id: 6,
+      brand: 'Britannia',
+      name: 'Britannia Little Hearts Classic Biscuits  (34.5 g)',
+      mrp: '10',
+      rs: '9',
+      off: '10',
+      img: 'https://rukminim1.flixcart.com/image/832/832/k8j3gcw0/cookie-biscuit/k/8/g/34-5-little-hearts-classic-biscuits-britannia-original-imafqgw9ngqaq69q.jpeg?q=70'
+    },
+    {
+      _id: 7,
       brand: 'Milkfood',
       name: 'Milkfood Rich Desi Daneder Ghee',
       mrp: '233',
       rs: '32',
-      off: '20'
+      off: '20',
+      img:'https://n3.sdlcdn.com/imgs/e/l/1/MSG-Cow-Desi-Ghee-1-SDL595875615-1-fddf8.png'
+    },
+    {
+      _id: 8,
+      brand: 'Fortune',
+      name: 'Fortune Sunlite Refined Sunflower Oil Pouch',
+      mrp: '120',
+      rs: '98',
+      off: '18',
+      img:'https://rukminim1.flixcart.com/image/832/832/k7usyvk0/edible-oil/e/t/c/1-sunlite-refined-pouch-sunflower-oil-fortune-original-imafpzwhyyhhvzbk.jpeg?q=70'
+    },
+    {
+      _id: 9,
+      brand: 'Hambre',
+      name: 'Hambre Raisin_Golden_250gm Raisins  (250 g)',
+      mrp: '430',
+      rs: '245',
+      off: '43',
+      img: 'https://rukminim1.flixcart.com/image/832/832/jp02t8w0/nut-dry-fruit/2/c/3/250-raisin-golden-250gm-pouch-hambre-original-imafb9yprzn7jrg8.jpeg?q=70'
+    },
+    {
+      _id: 10,
+      brand: 'TATA',
+      name: 'Tata Iodized Salt  (1 kg)',
+      mrp: '20',
+      rs: '18',
+      off: '10',
+      img: 'https://rukminim1.flixcart.com/image/832/832/jq4353k0/salt/q/w/m/1-na-iodized-salt-tata-original-imafc7agxkbypvgr.jpeg?q=70'
+    },
+    {
+      _id: 11,
+      brand: 'Cadbury',
+      name: 'Cadbury Bournvita Health Nutrition Drink  (500 g, Chocolate Flavored)',
+      mrp: '217',
+      rs: '210',
+      off: '3',
+      img: 'https://rukminim1.flixcart.com/image/832/832/jr0y9ow0/energy-sport-drink-mix/m/r/w/500-pro-health-chocolate-cadbury-bournvita-original-imafcx6zg3sazzzk.jpeg?q=70'
     }];
   }
 
